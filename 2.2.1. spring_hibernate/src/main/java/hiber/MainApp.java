1766ed88e2c2b4e2f2d2bc7794d6633e0212bf05
Anton <timurgess@yandex.ru>
2026-05-05 18:06:10 +0300

    add Hibernate OneToOne (User-Car) and HQL query

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -19,6 +20,15 @@ public class MainApp {
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car = new Car("BMW",5);
+      User user1 = new User("Anton","Sergeev","antoxna14888@yandex.ru");
+      user1.setCar(car);
+      userService.add(user1);
+
+      User foundUser = userService.getUserByCar("BMW", 5);
+
+      System.out.println("Найден пользователь:");
+      System.out.println(foundUser.getFirstName() + " " + foundUser.getLastName());
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,6 +36,9 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         if (user.getCar() != null) {
+            System.out.println("Car = " + user.getCar().getModel() + " " + user.getCar().getSeries());
+         }
          System.out.println();
       }
 
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,40 @@
+package hiber.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column
+    private String model;
+
+    @Column
+    int series;
+
+
+
+    public Car(){
+
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+}
